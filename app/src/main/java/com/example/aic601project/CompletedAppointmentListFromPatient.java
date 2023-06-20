RippleWave-Technologies/myTherapy

commit 4b8fcf86785a0dac81fb6a317450c9a7cc609a03

The commit implements the ModelAppointmentList class, which is designed to handle various DB retrieval operations and store the data on a hashmap, while also providing a mechanism to return the retrieved data to the calling activity. Also fixed an issue with fetchClinicConfirmedAppointmentsPastCurrent.php

app/src/main/java/com/example/aic601project/CompletedAppointmentListFromPatient.java
@@ -5,16 +5,16 @@ import com.example.aic601project.R3_R8.PhysicianMainActivity;
 import java.util.ArrayList;
 
 public class CompletedAppointmentListFromPatient {
-    private ArrayList<Appointment> appointments;
+    private ArrayList<ModelAppointment> appointments;
 
-    public CompletedAppointmentListFromPatient(String ip, Patient patient) {
+    public CompletedAppointmentListFromPatient(String ip, ModelPatient patient) {
         String url = "http://" + ip + "/myTherapy/fetchAppointments.php";
         try {
             OkHttpHandler okHttpHandler = new OkHttpHandler();
-            ArrayList<Appointment> tempApp = okHttpHandler.fetchCompletedAppoinments(url);
+            ArrayList<ModelAppointment> tempApp = okHttpHandler.fetchCompletedAppoinments(url);
 
-            for (Appointment app : tempApp){
-                if(app.getAFM().equals(PhysicianMainActivity.getAfm()) && app.getAmka().equals(patient.getAmka())){
+            for (ModelAppointment app : tempApp){
+                if(app.getAfm().equals(PhysicianMainActivity.getAfm()) && app.getAmka().equals(patient.getAmka())){
                     appointments.add(app);
                 }
             }
@@ -23,7 +23,7 @@ public class CompletedAppointmentListFromPatient {
         }
     }
 
-    public ArrayList<Appointment> getAppointments(){
+    public ArrayList<ModelAppointment> getAppointments(){
         return  this.appointments;
     }
 }

app/src/main/java/com/example/aic601project/ModelPatient.java
@@ -5,30 +5,22 @@ import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
 
-import com.example.aic601project.R3_R8.AppointmentInformationView;
-
 import java.util.ArrayList;
 
-public class Patient implements Parcelable {
-    private String amka;
-    private String name;
-    private String surname;
-    private String city;
-    private String address;
-    private String addressNumber;
-    private String postcode;
+public class ModelPatient implements Parcelable {
+    private String amka, name, surname, address, addressNumber, city, postCode;
 
-    private ArrayList<Appointment> completedAppointments;
+    private ArrayList<ModelAppointment> completedAppointments;
 
-    public Patient(String amka, String name, String surname, String city, String address, String addressNumber,
-            String postcode) {
+    public ModelPatient(String amka, String name, String surname, String city, String address, String addressNumber,
+                        String postCode) {
         this.amka = amka;
         this.name = name;
         this.surname = surname;
         this.city = city;
         this.address = address;
         this.addressNumber = addressNumber;
-        this.postcode = postcode;
+        this.postCode = postCode;
 
         CompletedAppointmentListFromPatient comApp = new CompletedAppointmentListFromPatient(MainActivity.getIP(), this);
 
@@ -36,25 +28,25 @@ public class Patient implements Parcelable {
 
     }
 
-    protected Patient(Parcel in) {
+    protected ModelPatient(Parcel in) {
         amka = in.readString();
         name = in.readString();
         surname = in.readString();
         city = in.readString();
         address = in.readString();
         addressNumber = in.readString();
-        postcode = in.readString();
+        postCode = in.readString();
     }
 
-    public static final Creator<Patient> CREATOR = new Creator<Patient>() {
+    public static final Creator<ModelPatient> CREATOR = new Creator<ModelPatient>() {
         @Override
-        public Patient createFromParcel(Parcel in) {
-            return new Patient(in);
+        public ModelPatient createFromParcel(Parcel in) {
+            return new ModelPatient(in);
         }
 
         @Override
-        public Patient[] newArray(int size) {
-            return new Patient[size];
+        public ModelPatient[] newArray(int size) {
+            return new ModelPatient[size];
         }
     };
 
@@ -108,11 +100,11 @@ public class Patient implements Parcelable {
     }
 
     public String getPostcode() {
-        return postcode;
+        return postCode;
     }
 
     public void setPostcode(String postcode) {
-        this.postcode = postcode;
+        this.postCode = postcode;
     }
 
     @Override
@@ -120,7 +112,7 @@ public class Patient implements Parcelable {
         return 0;
     }
 
-    public ArrayList<Appointment> getCompletedAppointments(){
+    public ArrayList<ModelAppointment> getCompletedAppointments(){
         return this.completedAppointments;
     }
 
@@ -132,6 +124,6 @@ public class Patient implements Parcelable {
         parcel.writeString(city);
         parcel.writeString(address);
         parcel.writeString(addressNumber);
-        parcel.writeString(postcode);
+        parcel.writeString(postCode);
     }
 }

app/src/main/java/com/example/aic601project/OkHttpHandler.java
@@ -1,13 +1,13 @@
 package com.example.aic601project;
 
 import android.os.StrictMode;
-import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import okhttp3.FormBody;
@@ -24,9 +24,9 @@ public class OkHttpHandler {
         StrictMode.setThreadPolicy(policy);
     }
 
-    ArrayList<Patient> fetchClinicPatients(String url, String afm) throws Exception {
+    ArrayList<ModelPatient> fetchClinicPatients(String url, String afm) throws Exception {
 
-        ArrayList<Patient> patients = new ArrayList<>();
+        ArrayList<ModelPatient> patients = new ArrayList<>();
 
         OkHttpClient client = new OkHttpClient().newBuilder().build();
         RequestBody body = new FormBody.Builder().add("afm", afm).build();
@@ -48,7 +48,7 @@ public class OkHttpHandler {
                 String addressNumber = patientObject.getString("addressNumber");
                 String postcode = patientObject.getString("postcode");
 
-                patients.add(new Patient(amka, name,surname,city, address, addressNumber, postcode));
+                patients.add(new ModelPatient(amka, name,surname,city, address, addressNumber, postcode));
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -57,9 +57,9 @@ public class OkHttpHandler {
         return patients;
     }
 
-    ArrayList<Appointment> fetchCompletedAppoinments(String url) throws Exception {
+    ArrayList<ModelAppointment> fetchCompletedAppoinments(String url) throws Exception {
 
-        ArrayList<Appointment> appoinments = new ArrayList<>();
+        ArrayList<ModelAppointment> appoinments = new ArrayList<>();
 
         OkHttpClient client = new OkHttpClient().newBuilder().build();
         RequestBody body = RequestBody.create("", MediaType.parse("text/plain"));
@@ -78,7 +78,7 @@ public class OkHttpHandler {
                 String status = json.get("status").toString();
                 String service = json.get("service").toString();
                 if (status.equals("3")){
-                    appoinments.add(new Appointment(date, amka,afm,status, service));
+                    appoinments.add(new ModelAppointment(date, amka,afm,status, service));
                 }
 
             }
@@ -110,7 +110,7 @@ public class OkHttpHandler {
                 String price = json.get("price").toString();
                 String description = json.get("description").toString();
 
-                services.add(new Service(code,name, price, description));
+                services.add(new Service(code, name, price, description));
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -154,8 +154,8 @@ public class OkHttpHandler {
         return clinics;
     }
 
-    public ArrayList<ModelAppointmentPFragment1> fetchClinicConfirmedAppointmentsPastCurrent(String url, String afm) throws IOException {
-        ArrayList<ModelAppointmentPFragment1> appointments = new ArrayList<>();
+    public HashMap<ModelAppointment, ModelPatient> fetchClinicConfirmedAppointmentsPastCurrent(String url, String afm) throws IOException {
+        HashMap<ModelAppointment, ModelPatient> appointmentData = new HashMap<>();
 
         OkHttpClient client = new OkHttpClient().newBuilder().build();
 
@@ -171,29 +171,27 @@ public class OkHttpHandler {
         Response response = client.newCall(request).execute();
         String data = response.body().string();
 
-        Log.d("imtestingrn", "im in1");
         try {
             JSONObject json = new JSONObject(data);
             Iterator<String> keys = json.keys();
-            Log.d("imtestingrn", "im in2");
             while (keys.hasNext()) {
-                Log.d("imtestingrn", "im in3");
                 String key = keys.next();
                 JSONObject clinicObject = json.getJSONObject(key);
 
+                String amka = key.substring(0, 10);
+                String date = key.substring(11);
+
                 String name = clinicObject.getString("name");
                 String surname = clinicObject.getString("surname");
-                String date = clinicObject.getString("date");
 
-                Log.d("imtestingrn", key + " " + name + " " + surname + " " + date);
-
-                appointments.add(new ModelAppointmentPFragment1(key, name, surname, date));
+                appointmentData.put(new ModelAppointment(amka, "", date, "", ""),
+                        new ModelPatient(amka, name, surname, "", "", "", ""));
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        return appointments;
+        return appointmentData;
     }
 
     public int insertOrUpdateClinic(String url, String afm, String name, String email, String address,

app/src/main/java/com/example/aic601project/Appointment.java
@@ -1,61 +1,0 @@
-package com.example.aic601project;
-
-public class Appointment {
-    private String date;
-    private String amka;
-    private String afm;
-    private String status;
-    private String service;
-
-    public Appointment(String date, String amka, String afm, String status, String service) {
-        this.date = date;
-        this.amka = amka;
-        this.afm = afm;
-        this.status = status;
-        this.service = service;
-    }
-
-
-
-    //Getters and Setters
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getAmka() {
-        return amka;
-    }
-
-    public void setAmka(String amka) {
-        this.amka = amka;
-    }
-
-    public String getAFM() {
-        return afm;
-    }
-
-    public void setAFM(String afm) {
-        this.afm = afm;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getService() {
-        return service;
-    }
-
-    public void setService(String service) {
-        this.service = service;
-    }
-
-}

app/src/main/java/com/example/aic601project/MainActivity.java
@@ -11,11 +11,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
     private static final String IP = "temp";
 
-    private PatientList patientList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        patientList = new PatientList(IP);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);

app/src/main/java/com/example/aic601project/ModelAppointment.java
@@ -1,0 +1,24 @@
+package com.example.aic601project;
+
+public class ModelAppointment {
+
+    private String amka, afm, date, status, service;
+
+    public ModelAppointment(String amka, String afm, String date, String status, String service) {
+        this.amka = amka;
+        this.afm = afm;
+        this.date = date;
+        this.status = status;
+        this.service = service;
+    }
+
+    public String getAmka() { return amka; }
+
+    public String getAfm() { return afm; }
+
+    public String getDate() { return date; }
+
+    public String getStatus() { return status; }
+
+    public String getService() { return service; }
+}

app/src/main/java/com/example/aic601project/ModelAppointmentList.java
@@ -1,0 +1,34 @@
+package com.example.aic601project;
+
+import android.util.Log;
+
+import java.util.HashMap;
+
+public class ModelAppointmentList {
+
+    HashMap<ModelAppointment, ModelPatient> appointmentData;
+
+    public ModelAppointmentList(String ip, String afm, String enterCase) {
+        appointmentData = new HashMap<>();
+
+        switch (enterCase) {
+            case "PhysicianFragment1":
+                String url = "http://" + ip + "/myTherapy/fetchClinicConfirmedAppointmentsPastCurrent.php";
+                try {
+                    OkHttpHandler okHttpHandler = new OkHttpHandler();
+                    appointmentData = okHttpHandler.fetchClinicConfirmedAppointmentsPastCurrent(url, afm);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "R4":
+                Log.d("R4", "R4");
+                break;
+        }
+
+    }
+
+    public HashMap<ModelAppointment, ModelPatient> getAppointmentData() {
+        return appointmentData;
+    }
+}

app/src/main/java/com/example/aic601project/ModelAppointmentListPFragment1.java
@@ -1,21 +1,0 @@
-package com.example.aic601project;
-
-import java.util.ArrayList;
-
-public class ModelAppointmentListPFragment1 {
-
-    private ArrayList<ModelAppointmentPFragment1> appointments;
-
-    public ModelAppointmentListPFragment1(String ip, String afm) {
-        appointments = new ArrayList<>();
-        String url = "http://" + ip + "/myTherapy/fetchClinicConfirmedAppointmentsPastCurrent.php";
-        try {
-            OkHttpHandler okHttpHandler = new OkHttpHandler();
-            appointments = okHttpHandler.fetchClinicConfirmedAppointmentsPastCurrent(url, afm);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public ArrayList<ModelAppointmentPFragment1> getAppointments() { return appointments; }
-}

commit fd17d55cdb4a976ff42bec912a80d257e1764611

Fixed bugs that were introduced with commits 4b8fcf8 and d141d2a.

app/src/main/java/com/example/aic601project/CompletedAppointmentListFromPatient.java
@@ -5,16 +5,16 @@ import com.example.aic601project.R3_R8.PhysicianMainActivity;
 import java.util.ArrayList;
 
 public class CompletedAppointmentListFromPatient {
-    private ArrayList<ModelAppointment> appointments;
+    private ArrayList<Appointment> appointments;
 
     public CompletedAppointmentListFromPatient(String ip, ModelPatient patient) {
         String url = "http://" + ip + "/myTherapy/fetchAppointments.php";
         try {
             OkHttpHandler okHttpHandler = new OkHttpHandler();
-            ArrayList<ModelAppointment> tempApp = okHttpHandler.fetchCompletedAppoinments(url);
+            ArrayList<Appointment> tempApp = okHttpHandler.fetchCompletedAppoinments(url);
 
-            for (ModelAppointment app : tempApp){
-                if(app.getAfm().equals(PhysicianMainActivity.getAfm()) && app.getAmka().equals(patient.getAmka())){
+            for (Appointment app : tempApp){
+                if(app.getAFM().equals(PhysicianMainActivity.getAfm()) && app.getAmka().equals(patient.getAmka())){
                     appointments.add(app);
                 }
             }
@@ -23,7 +23,7 @@ public class CompletedAppointmentListFromPatient {
         }
     }
 
-    public ArrayList<ModelAppointment> getAppointments(){
+    public ArrayList<Appointment> getAppointments(){
         return  this.appointments;
     }
 }

app/src/main/java/com/example/aic601project/ModelPatient.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class ModelPatient implements Parcelable {
     private String amka, name, surname, address, addressNumber, city, postCode;
 
-    private ArrayList<ModelAppointment> completedAppointments;
+    private ArrayList<Appointment> completedAppointments;
 
     public ModelPatient(String amka, String name, String surname, String city, String address, String addressNumber,
                         String postCode) {
@@ -112,7 +112,7 @@ public class ModelPatient implements Parcelable {
         return 0;
     }
 
-    public ArrayList<ModelAppointment> getCompletedAppointments(){
+    public ArrayList<Appointment> getCompletedAppointments(){
         return this.completedAppointments;
     }
 

app/src/main/java/com/example/aic601project/ModelService.java
@@ -1,12 +1,12 @@
 package com.example.aic601project;
 
-public class Service {
+public class ModelService {
     private String code;
     private String name;
     private String price;
     private String discription;
 
-    public Service(String code, String name, String price, String discription) {
+    public ModelService(String code, String name, String price, String discription) {
         this.code = code;
         this.name = name;
         this.price = price;

app/src/main/java/com/example/aic601project/ModelServiceList.java
@@ -2,10 +2,10 @@ package com.example.aic601project;
 
 import java.util.ArrayList;
 
-public class ServiceList {
-    private ArrayList<Service> services;
+public class ModelServiceList {
+    private ArrayList<ModelService> services;
 
-    public ServiceList(String ip) {
+    public ModelServiceList(String ip) {
         String url = "http://" + ip + "/myTherapy/fetchServices.php";
         try {
             OkHttpHandler okHttpHandler = new OkHttpHandler();

app/src/main/java/com/example/aic601project/OkHttpHandler.java
@@ -57,9 +57,9 @@ public class OkHttpHandler {
         return patients;
     }
 
-    ArrayList<ModelAppointment> fetchCompletedAppoinments(String url) throws Exception {
+    ArrayList<Appointment> fetchCompletedAppoinments(String url) throws Exception {
 
-        ArrayList<ModelAppointment> appoinments = new ArrayList<>();
+        ArrayList<Appointment> appoinments = new ArrayList<>();
 
         OkHttpClient client = new OkHttpClient().newBuilder().build();
         RequestBody body = RequestBody.create("", MediaType.parse("text/plain"));
@@ -75,11 +75,6 @@ public class OkHttpHandler {
 
                 String amka = json.get("amka").toString();
                 String afm = json.get("afm").toString();
-                String status = json.get("status").toString();
-                String service = json.get("service").toString();
-                if (status.equals("3")){
-                    appoinments.add(new ModelAppointment(date, amka,afm,status, service));
-                }
                 String service = json.get("serviceName").toString();
                 String lastName = json.get("surname").toString();
 
@@ -94,9 +89,9 @@ public class OkHttpHandler {
     }
 
 
-    ArrayList<Service> fetchServices(String url) throws Exception {
+    ArrayList<ModelService> fetchServices(String url) throws Exception {
 
-        ArrayList<Service> services = new ArrayList<>();
+        ArrayList<ModelService> services = new ArrayList<>();
 
         OkHttpClient client = new OkHttpClient().newBuilder().build();
         RequestBody body = RequestBody.create("", MediaType.parse("text/plain"));
@@ -114,7 +109,7 @@ public class OkHttpHandler {
                 String price = json.get("price").toString();
                 String description = json.get("description").toString();
 
-                services.add(new Service(code, name, price, description));
+                services.add(new ModelService(code, name, price, description));
             }
         } catch (JSONException e) {
             e.printStackTrace();

app/src/main/res/values/strings.xml
@@ -1,5 +1,5 @@
 <resources>
-    <string name="app_name">AIC601project</string>
+    <string name="app_name">myTherapy</string>
     <string name="company_name">myTherapy</string>
 
     <string name="login">Σύνδεση</string>